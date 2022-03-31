2e9bd0836c72e10ef1a9dead3e42e022f6875d27
unknown <mwright7272@gmail.com>
2022-03-31 18:39:53 -0500

    add js file and some pseduocode

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var containerEl = $(".container")
+var times = ['9AM','10AM','11AM','12AM','1PM','2PM','3PM','4PM','5PM']
+
+
+function createDiv() {
+    //create line with, potential task, and save button
+}
+
+
+function addDiv() {
+    //add div to page with for loop
+    //make an unordered list?
+    //consult taskinator/taskmaster pro
+}
+
