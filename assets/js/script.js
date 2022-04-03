ca2ef63e32a5acba4ba59b6a53fe81e45ff39aa0
unknown <mwright7272@gmail.com>
2022-04-03 12:43:04 -0500

    conditional highlighting based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
 var containerEl = $(".container")
-var times = ['9AM','10AM','11AM','12AM','1PM','2PM','3PM','4PM','5PM']
+var times = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM']
+var taskEl = document.querySelector('.task-container')
+var descriptionEl = $('.description')
+//console.log(descriptionEl.length);
+//console.log(descriptionEl[0])
+//console.log(descriptionEl[1])
+//console.log(descriptionEl[2])
 
+var auditTime = function () {
+    var currentHour = moment().hour();
+    console.log(currentHour);
 
-function createDiv() {
-    //create line with, potential task, and save button
+    for (i = 0; i < descriptionEl.length; i++) {
+        var currentTask = descriptionEl[i];
+        console.log(currentTask);
+
+        if (descriptionEl[i].dataset.time < currentHour) {
+            console.log("PAST")
+            $(currentTask).addClass('past')
+        }
+        else if (descriptionEl[i].dataset.time > currentHour) {
+            console.log("Future")
+            $(currentTask).addClass('future')
+        }
+        else if (descriptionEl[i].dataset.time = currentHour) {
+            console.log('Present')
+            $(currentTask).addClass('present')
+        }
+
+    }
 }
 
-
-function addDiv() {
-    //add div to page with for loop
-    //make an unordered list?
-    //consult taskinator/taskmaster pro
-}
+auditTime();
 
